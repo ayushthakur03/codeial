993758ff5326a9fe0b684933e811a83a7a598fde
ayush <ayushthakur2117@gmail.com>
2021-08-18 16:28:47 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express= require('express');
+const app= express();
+const port=8000;
+
+app.listen(port,function(err)
+{
+    if(err)
+    {
+        console.log(`Error due to: ${err}`);
+        return;
+    }
+    console.log(`Server running at port: ${port}`);
+});
